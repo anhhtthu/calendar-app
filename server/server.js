d081d3171dd82754dd27f97516773a4b4fd78327
anh.dophuong <anh.dophuong@buymed.com>
2023-11-01 17:52:42 +0700

    feat: implement logger custom builder

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const logger = require("./src/utils/logger");
 require("dotenv").config();
 
 const app = express();
@@ -10,10 +11,11 @@ app.use(cors());
 
 // Test route
 app.get("/", (req, res) => {
+  logger.infof("GET / route hit");
   res.send("Test Calendar App");
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  logger.infof('Server is running on http://localhost:%d', PORT)
 });
---
+++ server/src/utils/logger.js
@@ -0,0 +1,64 @@
+const { createLogger, format, transports } = require("winston");
+require("winston-daily-rotate-file");
+const sprintf = require("sprintf-js").sprintf;
+
+async function getStackTrace() {
+  const stackTrace = await import("stack-trace");
+  return stackTrace;
+}
+
+// Logs rotation
+const fileTransport = new transports.DailyRotateFile({
+  filename: "./logs/server-%DATE%.log",
+  datePattern: "YYYY-MM-DD",
+  zippedArchive: false,
+  maxSize: "20m",
+  maxFiles: "14d",
+});
+
+// Custom printed log format
+const customFormat = format.printf(({ timestamp, level, message }) => {
+  return `${timestamp} - ${level} - ${message}`;
+});
+
+// Create logger instance
+const logger = createLogger({
+  level: "info",
+  format: format.combine(
+    format.colorize(),
+    format.timestamp({
+      format: "YYYY-MM-DD HH:mm:ss",
+    }),
+    customFormat
+  ),
+  transports: [new transports.Console(), fileTransport],
+});
+
+// getCallerInfo() for tracing which log info/error is came from
+async function getCallerInfo() {
+  const stackTrace = await getStackTrace();
+  const trace = stackTrace.get();
+
+  const caller = trace[1];
+  if (!caller) {
+    return 'file-unknown:line-unknown';
+  }
+
+  const file = caller.getFileName();
+  const line = caller.getLineNumber();
+  return `${file}:${line}`;
+}
+
+logger.infof = async function (message, ...args) {
+  const callerInfo = await getCallerInfo();
+  const formattedMessage = sprintf(message, ...args);
+  this.info(`${callerInfo} - ${formattedMessage}`);
+};
+
+logger.errorf = async function (message, ...args) {
+  const callerInfo = await getCallerInfo();
+  const formattedMessage = sprintf(message, ...args);
+  this.error(`${callerInfo} - ${formattedMessage}`);
+};
+
+module.exports = logger;
