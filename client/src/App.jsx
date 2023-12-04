168356c1617b4e1fd3b28cebcce1127410f8cc9c
anh.dophuong <anh.dophuong@buymed.com>
2023-12-04 15:40:02 +0700

    merge conflict

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,26 +15,12 @@ import React from "react";
 function App() {
   const location = useLocation();
   return (
-<<<<<<< HEAD
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/userprofile" element={<UserProfile />} />
-      <Route path="/changepassword" element={<ChangePassword />} />
-      <Route path="/calendar" element={<Calendar />}>
-        <Route index element={<Navigate to="monthview" replace />} />
-        <Route path="monthview" element={<MonthViewCalendar />} />
-        <Route path="weekview" element={<WeekViewCalendar />} />
-        <Route path="dayview" element={<DayViewCalendar />} />
-        <Route path="yearview" element={<YearViewCalendar />} />
-      </Route>
-      <Route path="*" element={<Navigate to="/login" />} />
-    </Routes>
-=======
     <AnimatePresence wait>
       <Routes location={location} key={location.key}>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/userprofile" element={<UserProfile />} />
+        <Route path="/change-password" element={<ChangePassword />} />
         <Route path="/calendar" element={<Calendar />}>
           <Route index element={<Navigate to="monthview" replace />} />
           <Route path="monthview" element={<MonthViewCalendar />} />
@@ -45,7 +31,6 @@ function App() {
         <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </AnimatePresence>
->>>>>>> c0aea91e50fd967ec1bf9035760b17b1367063ea
   );
 }
 
